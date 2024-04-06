1321457cf215f158fa5e45e2a3c8f55fb717238e
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-04-06 14:28:47 +0200

    new2

--- index.js
+++ index.js
@@ -138,17 +138,17 @@
  *? Функція рахує та повертає загальну вартість каменів
  *? з таким ім'ям, ціною та кількістю з об'єкта
  */
-const stones = [
-  { name: "Изумруд", price: 1300, quantity: 4 },
-  { name: "Бриллиант", price: 2700, quantity: 6 },
-  { name: "Сапфир", price: 400, quantity: 7 },
-  { name: "Щебень", price: 150, quantity: 100 },
-];
-function calcTotalPrice(someStones, stoneName) {
-  for (let key of stones) {
-    if (key.name === stoneName) {
-      return key.price * key.quantity;
-    }
-  }
-}
-console.log(calcTotalPrice(stones, "Изумруд"));
+// const stones = [
+//   { name: "Изумруд", price: 1300, quantity: 4 },
+//   { name: "Бриллиант", price: 2700, quantity: 6 },
+//   { name: "Сапфир", price: 400, quantity: 7 },
+//   { name: "Щебень", price: 150, quantity: 100 },
+// ];
+// function calcTotalPrice(someStones, stoneName) {
+//   for (let key of stones) {
+//     if (key.name === stoneName) {
+//       return key.price * key.quantity;
+//     }
+//   }
+// }
+// console.log(calcTotalPrice(stones, "Изумруд"));
