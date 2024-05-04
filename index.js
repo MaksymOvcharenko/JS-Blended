1b0d1dd62632c894b679baacf95d0f3702dfed96
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-05-04 13:34:41 +0200

    3

--- index.js
+++ index.js
@@ -1,316 +1,49 @@
-/**
- *? Використовуючи функцію if...else,
- *? напишіть код, який запитуватиме:
- *? "Яка офіційна назва JavaScript?"
- *? Якщо користувач вводить "ECMAScript",
- *? то показати через alert: "Вірно!"
- *? інакше відобразити:"Не знаєте? ECMAScript!"
- */
-// const answer = prompt("Яка офіційна назва JavaScript?");
-// console.log(answer);
-// function answerCheck(string) {
-//   if (string === "ECMAScript") {
-//     alert("Вірно!");
-//   } else {
-//     alert("Не знаєте? ECMAScript!");
-//   }
-// }
-// answerCheck(answer);
-
-/**
-Напишіть програму, яка отримає від користувача
- число (кількість хвилин) та виведе в консоль
- рядок у форматі годин та хвилин
- https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
- 70 === 01:10
- */
-
-// const time = +prompt("Введіть кількість хвилин:");
-// console.log(time);
-// function convertTime(number) {
-//   const hours = String(Math.floor(number / 60)).padStart(2, 0);
-//   console.log(hours);
-//   const minutes = String(number % 60).padStart(2, 0);
-//   console.log(minutes);
-//   console.log(`${hours}:${minutes}`);
-// }
-// convertTime(time);
-
-/**
- *? Напишіть цикл, який виводить у консоль
- *? всі непарні числа  від max до min  за спаданням
- */
-// const a = 50;
-// const b = 23;
-// function sowNamber(max, min) {
-//   for (let i = max; i >= min; i--) {
-//     if (i % 2 !== 0) {
-//       console.log(i);
-//     }
-//   }
-// }
-// sowNamber(a, b);
-
-/**
- *? За допомогою циклу for складіть усі парні числа від min до max
- */
-
-// const max = 50;
-// const min = 0;
-
-// function culcTotal(max, min) {
-//   let total = 0;
-//   for (let i = min; i <= max; i++) {
-//     if (!(i % 2)) {
-//       total += i;
-//     }
-//   }
-//   return total;
-// }
-// const result = culcTotal(max, min);
-// console.log(result);
-
-/**
- *? У змінній min лежить число від 0 до 59.
- *? Визначте, в яку чверть години потрапляє
- *? це число (у першу, другу, третю чи четверту).
- */
-// const min = 11;
-// function partNumber(number) {
-//     if (number > 0 && number <= 15) {
-//         console.log ("first part")
-//     } else if (number > 15 && number <= 30) {
-//         console.log ("second part")
-//     }
-//     else if (number > 30 && number <= 45) {
-//         console.log ("third part")
-//     }
-//     else {
-//         console.log ("fourth part")
-//     }
-// }
-// partNumber(min);
-
-/**
- *? Напиши скрипт для об'єкта user,
- *? послідовно:
- *? 1 додати поле mood зі значенням 'happy'
- *? 2 замінить hobby на 'skydiving'
- *? 3 замінить значення premium на false
- *? 4 виводить вміст об'єкта users у форматі
- *? ключ: значення використовуючи Object.keys() і for...of
- */
-// const user = {
-//   name: "John",
-//   age: 20,
-//   hobby: "tennis",
-//   premium: true,
-// };
-
-// user.mood = "happy";
-// user.hobby = "skydiving";
-// user.premium = false;
-
-// const keys = Object.keys(user);
-
-// for (const key of keys) {
-//   console.log(`${key}: ${user[key]}`);
-// }
-/* Напишіть функцію, яка приймає об'єкт і рахує загальну суму зарплат працівників. */
-// const someObj = {
-//   worker1: 360,
-//   worker2: 750,
-//   worker3: 240,
-// };
-// function culcTotalSalary(obj) {
-//   let sum = 0;
-//   const valeus = Object.values(obj);
-//   for (const item of valeus) {
-//     sum += item;
-//   }
-//   return sum;
-// }
-// console.log(culcTotalSalary(someObj));
-/**
- *? Напишіть ф-цію calcTotalPrice(someStones, stoneName),
- *? яка приймає масив об'єктів та
- *? рядок під назвою каменю.
- *? Функція рахує та повертає загальну вартість каменів
- *? з таким ім'ям, ціною та кількістю з об'єкта
- */
-// const stones = [
-//   { name: "Изумруд", price: 1300, quantity: 4 },
-//   { name: "Бриллиант", price: 2700, quantity: 6 },
-//   { name: "Сапфир", price: 400, quantity: 7 },
-//   { name: "Щебень", price: 150, quantity: 100 },
-// ];
-// function calcTotalPrice(someStones, stoneName) {
-//   for (let key of stones) {
-//     if (key.name === stoneName) {
-//       return key.price * key.quantity;
-//     }
-//   }
-// }
-// console.log(calcTotalPrice(stones, "Изумруд"));
-
-///**
-// *? Напишіть функцію, яка приймає массив об'єктів і повертає новий массив
-// *? Зробіть знижку 20 % на всі фрукти у масиві
-// *? Надайте ід для кожного продукту
-// */
-// const fruits = [
-//     { name: "apple", price: 200 },
-//     { name: "orange", price: 300 },
-//     { name: "grapes", price: 750 },
-// ];
-
-// function updateFruits(array) {
-//     let newArr = [];
-//     let id = 1;
-
-//     for (const item of array) {
-//         const newFruit = { ...item, price: item.price * 0.8, id }
-//         newArr.push(newFruit);
-//         id += 1;
-
-//     }
-
-//     return newArr;
-// }
-
-// console.log(updateFruits(fruits));
-// console.log(fruits);
-
-// напиши код який із массиву arr видаляє елемент foo.
-// розвертає цей массив
-// і повертає рядок js is the best
-// вхідний массив не має змінюватись (мутується)
-
-// const arr = ["best", "the", "foo", "is", "js"];
-// const newArray = [...arr];
-// const idx = newArray.indexOf("foo");
-// newArray.splice(idx, 1);
-// newArray.reverse();
-// console.log(newArray.join(" "));
-// console.log(arr);
-
-// const string = arr
-//   .slice(0, arr.indexOf("foo"))
-//   .concat(arr.slice(arr.indexOf("foo") + 1, arr.length))
-//   .reverse()
-//     .join(" ");
-// console.log(string);
-
-/*Напишіть функцію checkBrackets(str) яка приймає рядок жс коду (someFn)
-  і перевіряє правильність закриття дужок () {} []
-  Якщо рядок містить коректний код функція повертає true.
-  В іншому випадку повертає false
+//TODO:==============================================
+/*
+Завдання 1
+Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
 */
-// const someFn = `function foo() {
-//   const arr = [1, 2, 3];
-//   console.log(arr);
-// }`;
-// console.log(checkBrackets(someFn));
-// function checkBrackets(str) {
-//   const stack = [];
-//   const obj = {
-//     "(": ")",
-//     "{": "}",
-//     "[": "]",
-//   };
-//   for (let i = 0; i < str.length; i++) {
-//     const bracket = str[i];
-//     if (bracket === "(" || bracket === "{" || bracket === "[") {
-//       stack.push(bracket);
-//     }
-//     if (bracket === "}" || bracket === ")" || bracket === "]") {
-//       const lastEl = stack.pop();
-//       if (bracket !== obj[lastEl]) {
-//         return false;
-//       }
-//     }
-//   }
-//   if (stack.length) return false;
-//   return true;
-// }
-// console.log(checkBrackets(someFn));
-///**
 
-// *? Напишіть функцію, яка приймає массив об'єктів і повертає новий массив
-// *? Зробіть знижку 20 % на всі фрукти у масиві
-// *? Надайте ід для кожного продукту
-// */
-// const fruits = [
-//   { name: "apple", price: 200 },
-//   { name: "orange", price: 300 },
-//   { name: "grapes", price: 750 },
-// ];
-// function updateFruits(arr) {
-//  return arr.map((fruit, index) => ({...fruit, price: fruit.price * 0.8, id: index+1}))
-// }
+//TODO:==============================================
+/*
+Завдання 2
+Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+*/
 
-// console.log(updateFruits(fruits));
+//TODO:==============================================
+/*
+Завдання 3
+Кнопка "Приховати" ховає текст і замінює назву кнопки на
+"Розкрити", при повторному натисканні текст знову стає доступним
+і кнопка набуває початкового вигляду.
+*/
 
-// //TODO:==============================================
-// /**
-//  *? Поверніть об'єкт, в якому вказано кількість тегів.
-//  *? Очікуваний результат {js: 3, nodejs: 3, html: 2, css: 2, react: 2}
-//  */
-// const tweets = [
-//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
-//   { id: "001", likes: 2, tags: ["html", "css"] },
-//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-//   { id: "003", likes: 8, tags: ["css", "react"] },
-//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-// ];
-// function calcTags(arr) {
-//   return arr
-//     .flatMap((item) => item.tags)
-//     .reduce((acc, item) => {
-//       return { ...acc, [item]: acc[item] ? acc[item] + 1 : 1 };
-//     }, {});
-// }
-// console.log(calcTags(tweets));
+//TODO:==============================================
+/*
+Завдання 4
+Кнопка "Зменшити" робить квадрат менше на 10 пікселів,  "Збільшити" - більше на 10 пікселів.
+*/
 
-/**
-//  *? З об'єкту concerts потрібно отримати масив
-//  *? в якому будуть лише імена міст.
-//  *? З масиву потрібно прибрати міста, в яких концерт уже пройшов і
-//  *? відсортувати їх у хронологічному порядку.
-//  *? Результат вивести у консоль.
-//  *? Очікуваний результат ["Одеса", "Умань", "Харків"]
-//  */
-// const concerts = {
-//   Київ: new Date("2020-04-01"),
-//   Умань: new Date("2025-07-02"),
-//   Вінниця: new Date("2020-04-21"),
-//   Одеса: new Date("2025-03-15"),
-//   Хмельницький: new Date("2020-04-18"),
-//   Харків: new Date("2025-07-10"),
-// };
+//TODO:==============================================
+/*
+Завдання 5
+Додайте слухач кліку на window і визначте чи клікнув користувач у дів з id="place".
+*/
 
-// const array = Object.keys(concerts);
-// console.log(array);
+//TODO:==============================================
+/*
+Завдання 6
+Натиснувши кнопку "Подвоювати", збільшити значення
+у кожному елементі списку у 2 рази
+*/
 
-// const filteredArray = array.filter((item) => concerts[item] > new Date());
-// console.log(filteredArray);
-
-// const toSortedArray = filteredArray.toSorted(
-//   (a, b) => concerts[a] - concerts[b]
-// );
-// console.log(toSortedArray);
-
-//TODO:=============================================
-/**
- *? Напиши клас Notes який управляє колекцією нотаток у
- *? властивості items.
- *? Нотатка - це об'єкт із властивостями id, text і priority.
- *? Додай класу статичний метод Priopity,
- *? який буде повертати об'єкт із пріоритетами 
-//  {
-//       HIGHT: "hight",
-//       LOW: "low",
-//     }
- *? Додай методи addNote(note), removeNote(id)
- *? updatePriority(id, newPriority)
- */
+//TODO:==============================================
+/*
+Завдання 7
+При натисканні на коло він повинен слідувати за курсором.
+При повторному натисканні він стає в початкове положення.
+https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent
+https://developer.mozilla.org/ru/docs/Web/API/MouseEvent/pageX
+https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageY
+*/
---
+++ oldBlended/index.js
@@ -0,0 +1,316 @@
+/**
+ *? Використовуючи функцію if...else,
+ *? напишіть код, який запитуватиме:
+ *? "Яка офіційна назва JavaScript?"
+ *? Якщо користувач вводить "ECMAScript",
+ *? то показати через alert: "Вірно!"
+ *? інакше відобразити:"Не знаєте? ECMAScript!"
+ */
+// const answer = prompt("Яка офіційна назва JavaScript?");
+// console.log(answer);
+// function answerCheck(string) {
+//   if (string === "ECMAScript") {
+//     alert("Вірно!");
+//   } else {
+//     alert("Не знаєте? ECMAScript!");
+//   }
+// }
+// answerCheck(answer);
+
+/**
+Напишіть програму, яка отримає від користувача
+ число (кількість хвилин) та виведе в консоль
+ рядок у форматі годин та хвилин
+ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ 70 === 01:10
+ */
+
+// const time = +prompt("Введіть кількість хвилин:");
+// console.log(time);
+// function convertTime(number) {
+//   const hours = String(Math.floor(number / 60)).padStart(2, 0);
+//   console.log(hours);
+//   const minutes = String(number % 60).padStart(2, 0);
+//   console.log(minutes);
+//   console.log(`${hours}:${minutes}`);
+// }
+// convertTime(time);
+
+/**
+ *? Напишіть цикл, який виводить у консоль
+ *? всі непарні числа  від max до min  за спаданням
+ */
+// const a = 50;
+// const b = 23;
+// function sowNamber(max, min) {
+//   for (let i = max; i >= min; i--) {
+//     if (i % 2 !== 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+// sowNamber(a, b);
+
+/**
+ *? За допомогою циклу for складіть усі парні числа від min до max
+ */
+
+// const max = 50;
+// const min = 0;
+
+// function culcTotal(max, min) {
+//   let total = 0;
+//   for (let i = min; i <= max; i++) {
+//     if (!(i % 2)) {
+//       total += i;
+//     }
+//   }
+//   return total;
+// }
+// const result = culcTotal(max, min);
+// console.log(result);
+
+/**
+ *? У змінній min лежить число від 0 до 59.
+ *? Визначте, в яку чверть години потрапляє
+ *? це число (у першу, другу, третю чи четверту).
+ */
+// const min = 11;
+// function partNumber(number) {
+//     if (number > 0 && number <= 15) {
+//         console.log ("first part")
+//     } else if (number > 15 && number <= 30) {
+//         console.log ("second part")
+//     }
+//     else if (number > 30 && number <= 45) {
+//         console.log ("third part")
+//     }
+//     else {
+//         console.log ("fourth part")
+//     }
+// }
+// partNumber(min);
+
+/**
+ *? Напиши скрипт для об'єкта user,
+ *? послідовно:
+ *? 1 додати поле mood зі значенням 'happy'
+ *? 2 замінить hobby на 'skydiving'
+ *? 3 замінить значення premium на false
+ *? 4 виводить вміст об'єкта users у форматі
+ *? ключ: значення використовуючи Object.keys() і for...of
+ */
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tennis",
+//   premium: true,
+// };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const keys = Object.keys(user);
+
+// for (const key of keys) {
+//   console.log(`${key}: ${user[key]}`);
+// }
+/* Напишіть функцію, яка приймає об'єкт і рахує загальну суму зарплат працівників. */
+// const someObj = {
+//   worker1: 360,
+//   worker2: 750,
+//   worker3: 240,
+// };
+// function culcTotalSalary(obj) {
+//   let sum = 0;
+//   const valeus = Object.values(obj);
+//   for (const item of valeus) {
+//     sum += item;
+//   }
+//   return sum;
+// }
+// console.log(culcTotalSalary(someObj));
+/**
+ *? Напишіть ф-цію calcTotalPrice(someStones, stoneName),
+ *? яка приймає масив об'єктів та
+ *? рядок під назвою каменю.
+ *? Функція рахує та повертає загальну вартість каменів
+ *? з таким ім'ям, ціною та кількістю з об'єкта
+ */
+// const stones = [
+//   { name: "Изумруд", price: 1300, quantity: 4 },
+//   { name: "Бриллиант", price: 2700, quantity: 6 },
+//   { name: "Сапфир", price: 400, quantity: 7 },
+//   { name: "Щебень", price: 150, quantity: 100 },
+// ];
+// function calcTotalPrice(someStones, stoneName) {
+//   for (let key of stones) {
+//     if (key.name === stoneName) {
+//       return key.price * key.quantity;
+//     }
+//   }
+// }
+// console.log(calcTotalPrice(stones, "Изумруд"));
+
+///**
+// *? Напишіть функцію, яка приймає массив об'єктів і повертає новий массив
+// *? Зробіть знижку 20 % на всі фрукти у масиві
+// *? Надайте ід для кожного продукту
+// */
+// const fruits = [
+//     { name: "apple", price: 200 },
+//     { name: "orange", price: 300 },
+//     { name: "grapes", price: 750 },
+// ];
+
+// function updateFruits(array) {
+//     let newArr = [];
+//     let id = 1;
+
+//     for (const item of array) {
+//         const newFruit = { ...item, price: item.price * 0.8, id }
+//         newArr.push(newFruit);
+//         id += 1;
+
+//     }
+
+//     return newArr;
+// }
+
+// console.log(updateFruits(fruits));
+// console.log(fruits);
+
+// напиши код який із массиву arr видаляє елемент foo.
+// розвертає цей массив
+// і повертає рядок js is the best
+// вхідний массив не має змінюватись (мутується)
+
+// const arr = ["best", "the", "foo", "is", "js"];
+// const newArray = [...arr];
+// const idx = newArray.indexOf("foo");
+// newArray.splice(idx, 1);
+// newArray.reverse();
+// console.log(newArray.join(" "));
+// console.log(arr);
+
+// const string = arr
+//   .slice(0, arr.indexOf("foo"))
+//   .concat(arr.slice(arr.indexOf("foo") + 1, arr.length))
+//   .reverse()
+//     .join(" ");
+// console.log(string);
+
+/*Напишіть функцію checkBrackets(str) яка приймає рядок жс коду (someFn)
+  і перевіряє правильність закриття дужок () {} []
+  Якщо рядок містить коректний код функція повертає true.
+  В іншому випадку повертає false
+*/
+// const someFn = `function foo() {
+//   const arr = [1, 2, 3];
+//   console.log(arr);
+// }`;
+// console.log(checkBrackets(someFn));
+// function checkBrackets(str) {
+//   const stack = [];
+//   const obj = {
+//     "(": ")",
+//     "{": "}",
+//     "[": "]",
+//   };
+//   for (let i = 0; i < str.length; i++) {
+//     const bracket = str[i];
+//     if (bracket === "(" || bracket === "{" || bracket === "[") {
+//       stack.push(bracket);
+//     }
+//     if (bracket === "}" || bracket === ")" || bracket === "]") {
+//       const lastEl = stack.pop();
+//       if (bracket !== obj[lastEl]) {
+//         return false;
+//       }
+//     }
+//   }
+//   if (stack.length) return false;
+//   return true;
+// }
+// console.log(checkBrackets(someFn));
+///**
+
+// *? Напишіть функцію, яка приймає массив об'єктів і повертає новий массив
+// *? Зробіть знижку 20 % на всі фрукти у масиві
+// *? Надайте ід для кожного продукту
+// */
+// const fruits = [
+//   { name: "apple", price: 200 },
+//   { name: "orange", price: 300 },
+//   { name: "grapes", price: 750 },
+// ];
+// function updateFruits(arr) {
+//  return arr.map((fruit, index) => ({...fruit, price: fruit.price * 0.8, id: index+1}))
+// }
+
+// console.log(updateFruits(fruits));
+
+// //TODO:==============================================
+// /**
+//  *? Поверніть об'єкт, в якому вказано кількість тегів.
+//  *? Очікуваний результат {js: 3, nodejs: 3, html: 2, css: 2, react: 2}
+//  */
+// const tweets = [
+//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//   { id: "001", likes: 2, tags: ["html", "css"] },
+//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//   { id: "003", likes: 8, tags: ["css", "react"] },
+//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
+// function calcTags(arr) {
+//   return arr
+//     .flatMap((item) => item.tags)
+//     .reduce((acc, item) => {
+//       return { ...acc, [item]: acc[item] ? acc[item] + 1 : 1 };
+//     }, {});
+// }
+// console.log(calcTags(tweets));
+
+/**
+//  *? З об'єкту concerts потрібно отримати масив
+//  *? в якому будуть лише імена міст.
+//  *? З масиву потрібно прибрати міста, в яких концерт уже пройшов і
+//  *? відсортувати їх у хронологічному порядку.
+//  *? Результат вивести у консоль.
+//  *? Очікуваний результат ["Одеса", "Умань", "Харків"]
+//  */
+// const concerts = {
+//   Київ: new Date("2020-04-01"),
+//   Умань: new Date("2025-07-02"),
+//   Вінниця: new Date("2020-04-21"),
+//   Одеса: new Date("2025-03-15"),
+//   Хмельницький: new Date("2020-04-18"),
+//   Харків: new Date("2025-07-10"),
+// };
+
+// const array = Object.keys(concerts);
+// console.log(array);
+
+// const filteredArray = array.filter((item) => concerts[item] > new Date());
+// console.log(filteredArray);
+
+// const toSortedArray = filteredArray.toSorted(
+//   (a, b) => concerts[a] - concerts[b]
+// );
+// console.log(toSortedArray);
+
+//TODO:=============================================
+/**
+ *? Напиши клас Notes який управляє колекцією нотаток у
+ *? властивості items.
+ *? Нотатка - це об'єкт із властивостями id, text і priority.
+ *? Додай класу статичний метод Priopity,
+ *? який буде повертати об'єкт із пріоритетами 
+//  {
+//       HIGHT: "hight",
+//       LOW: "low",
+//     }
+ *? Додай методи addNote(note), removeNote(id)
+ *? updatePriority(id, newPriority)
+ */
